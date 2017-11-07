3be06673b11ce75183a5b3baf374e44cfd20f167
lapdamptimen <efremovslavik2000@gmail.com>
2017-11-07 21:54:54 +0200

    dates validation

--- controllers/api/product.js
+++ controllers/api/product.js
@@ -1,19 +1,31 @@
 exports.get = (req, res) => {
-	let product_id = req.params.product_id;
+	let { product_id } = req.params;
+	// let product_id = req.params.product_id;
+
+	let { start_date, end_date } = req.query;
 
 	console.log('PRODUCT_ID', product_id);
-	console.log('GET', req.query.start_date, req.query.end_date);
+	console.log('GET', start_date, end_date);
 
-	res.status(200).json({
-		name: 'product_name',
-		price: {
-			current: 0,
-			daily: [
-				{
-					value: 0,
-					date: '2017-10-07'
-				}
-			]
-		}
-	});
+	let start_timestamp = Date.parse(start_date);
+	let end_timestamp = Date.parse(end_date);
+
+	if (isNaN(start_timestamp)) {
+		res.status(400).json({ message: 'start_date is invalid' });
+	} else if (isNaN(end_timestamp)) {
+		res.status(400).json({ message: 'end_date is invalid' });
+	} else {
+		res.status(200).json({
+			name: 'product_name',
+			price: {
+				current: 0,
+				daily: [
+					{
+						value: 0,
+						date: '2017-10-07'
+					}
+				]
+			}
+		});
+	}
 };
