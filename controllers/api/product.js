3c2c6d44df6d0d68d5a9a85d60de2903ce9290a6
lapdamptimen <efremovslavik2000@gmail.com>
2017-11-09 17:13:41 +0200

    minor fixes

--- controllers/api/product.js
+++ controllers/api/product.js
@@ -1,6 +1,5 @@
 exports.get = (req, res) => {
 	let { product_id } = req.params;
-	// let product_id = req.params.product_id;
 
 	let { start_date, end_date } = req.query;
 
@@ -10,10 +9,18 @@ exports.get = (req, res) => {
 	let start_timestamp = Date.parse(start_date);
 	let end_timestamp = Date.parse(end_date);
 
+	let errors = [];
+
 	if (isNaN(start_timestamp)) {
-		res.status(400).json({ message: 'start_date is invalid' });
-	} else if (isNaN(end_timestamp)) {
-		res.status(400).json({ message: 'end_date is invalid' });
+		errors.push({ message: 'start_date is invalid' });
+	}
+
+	if (isNaN(end_timestamp)) {
+		errors.push({ message: 'end_date is invalid' });
+	}
+
+	if (errors.length > 0) {
+		res.status(400).json({ errors });
 	} else {
 		res.status(200).json({
 			name: 'product_name',
